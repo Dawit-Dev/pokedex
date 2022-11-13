f2e15d45258f16e532964fae62e255edbf766396
Dawit Abraha <akdawit1@gmail.com>
2022-11-13 16:24:18 +0000

    Removed some content

--- src/App.js
+++ src/App.js
@@ -10,55 +10,7 @@ function App() {
       <Logo />
       <BestPokemon />
       <CaughtPokemon />
-
-      <Mentor />
-      <Weather />
-      <Name />
-      <MentorsList />
     </div>
-  );
-}
-
-
-
-
-////////////
-function Mentor() {
-  const mentors = ["Ali", "Kash", "Davide", "German", "Gerald"];
-  return <span>{mentors.join(", ")}</span>;
-}
-function Weather() {
-  const weatherData = {
-    temperature: 5,
-    location: "London",
-  };
-
-  return (
-    <p>
-      The temperature in {weatherData.location} is {weatherData.temperature}
-    </p>
-  );
-}
-function formatName(user) {
-  return user.firstName + " " + user.lastName;
-}
-
-function Name() {
-  const user = {
-    firstName: "Bob",
-    lastName: "Marley",
-  };
-  return <span>{formatName(user)}</span>;
-}
-const mentors = ["Ali", "Kash", "Davide", "German", "Gerald"];
-
-function MentorsList() {
-  return (
-    <ul>
-      {mentors.map((name) => (
-        <li>{name}</li>
-      ))}
-    </ul>
   );
 }
 
